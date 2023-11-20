a689298959dce6abb0b25aa4174c22b19b015960
YogaSariUkirr <agus.rockiler@gmail.com>
2023-11-20 15:06:28 +0800

    first commit

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
     <header>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark p-3">
             <div class="container">
-                <a class="navbar-brand" href="#">Pemrograman Web: 2101020070_agusadi</a>
+                <a class="navbar-brand" href="#">Pemrograman Web: 2101020039_angga</a>
                 <button class="btn btn-dark" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
                     Sidebar
                 </button>
--- kategori.php
+++ kategori.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$database = "2101020070_agusadi";
+$database = "2101020039_angga";
 
 // Membuat koneksi
 $conn = mysqli_connect($servername, $username, $password, $database);
