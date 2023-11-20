db7be7c391dc853e7ed71599d9703328bcb4e5bf
yogasariukir <99500294+agusadi15@users.noreply.github.com>
2023-11-20 15:34:46 +0800

    Add files via upload

---
+++ tugaswebngakan/sql.php
@@ -0,0 +1,10 @@
+<?php
+
+	$server		= "localhost"; 
+	$user		= "root";
+	$password	= ""; 
+	$database	= "2101020062_herrysuputra"; 
+
+	$con = mysqli_connect($server,$user,$password,$database);
+
+?>
